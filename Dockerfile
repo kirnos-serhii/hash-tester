74fd1f29b1078f7bf6467a40f30fed5bcc98e0d8
kirnos-serhii <kirnosserhii@gmail.com>
2020-12-22 14:04:35 +0200

    Refactored error handling

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ ARG JAR_FILE=target/*.jar
 
 COPY ${JAR_FILE} app.jar
 
+VOLUME /logs
+
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/khpi/demo/configuration/ErrorHandler.java
+++ src/main/java/com/khpi/demo/configuration/ErrorHandler.java
@@ -1,6 +1,7 @@
 package com.khpi.demo.configuration;
 
 import com.khpi.demo.exception.HashAlgorithmTesterRuntimeException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,17 +9,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(HashAlgorithmTesterRuntimeException.class)
     public ResponseEntity<List<String>> noRecipientsException(HashAlgorithmTesterRuntimeException exception) {
+        log.error("internal_server_error", exception);
         return ResponseEntity.badRequest().body(Collections.singletonList(exception.getMessage()));
     }
 
-//    @ExceptionHandler(Throwable.class)
-//    public ResponseEntity<List<String>> throwable(Throwable exception) {
-//        System.out.println(exception.getMessage());
-//        return ResponseEntity.badRequest().body(Collections.singletonList(exception.getMessage()));
-//    }
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<List<String>> throwable(Throwable exception) {
+        log.error("internal_server_error", exception);
+        System.out.println(exception.getMessage());
+        return ResponseEntity.badRequest().body(Collections.singletonList(exception.getMessage()));
+    }
 }
